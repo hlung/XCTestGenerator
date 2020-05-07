76395a119c0b25d97771af111274608783ea2eb6
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2020-05-07 17:24:54 +0800

    Add code

--- Sources/XCTestGenerator/XCTestGenerator.swift
+++ Sources/XCTestGenerator/XCTestGenerator.swift
@@ -1,3 +1,55 @@
-struct XCTestGenerator {
-    var text = "Hello, World!"
+import Foundation
+
+class XCTestGenerator {
+  class func generateXCAssertEqual(for variable: Any, name: String) -> String {
+    Mirror(reflecting: variable).generateXCAssertEqual(variableName: name).joined(separator: "\n")
+  }
 }
+
+private extension Mirror {
+
+  // Generates XCTAssertEqual(...) code from receiver
+  // --- Input: ---
+  //   class Episode {
+  //     let number: Int = 42
+  //     let someNil: String? = nil
+  //   }
+  // --- Output: ---
+  //   XCTAssertEqual(output.number, 42)
+  //   XCTAssertEqual(output.someNil, nil)
+  func generateXCAssertEqual(variableName: String) -> [String] {
+    var output: [String] = []
+
+    if let superclassMirror = superclassMirror {
+      output += superclassMirror.generateXCAssertEqual(variableName: variableName)
+      output += [""]
+    }
+
+    output += ["// \(subjectType)"]
+
+    for c in children {
+      guard var name = c.label else { continue }
+      name = variableName + "." + name
+
+      let value = c.value
+      if let string = value as? String {
+        output += ["XCTAssertEqual(\(name), \"\(string)\")"]
+      }
+      else if let date = value as? Date {
+        output += ["XCTAssertEqual(\(name), Date(timeIntervalSince1970: \(date.timeIntervalSince1970)))"]
+      }
+      else {
+        // Replace "Optional(x)" with "x"
+        let string = "\(value)".replacingOccurrences(of: "Optional\\((.+)\\)", with: "$1", options: .regularExpression)
+        output += ["XCTAssertEqual(\(name), \(string))"]
+      }
+    }
+
+    return output
+  }
+
+}
+
+//func isEquatable(_ value: Any) -> Bool {
+//  return (value as? AnyObject)?.conforms(to: protocol(Hashable)) ?? false
+//}
--- Tests/XCTestGeneratorTests/XCTestGeneratorTests.swift
+++ Tests/XCTestGeneratorTests/XCTestGeneratorTests.swift
@@ -2,14 +2,57 @@ import XCTest
 @testable import XCTestGenerator
 
 final class XCTestGeneratorTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(XCTestGenerator().text, "Hello, World!")
-    }
+  func testExample() {
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    let output = Episode()
+    let string = XCTestGenerator.generateXCAssertEqual(for: output, name: "output")
+    //print(string)
+    XCTAssertEqual(string, """
+      // Media
+      XCTAssertEqual(output.name, "Some media")
+      XCTAssertEqual(output.array, ["foo", "bar"])
+      XCTAssertEqual(output.stringDict, ["es": "Hola", "en": "Hi"])
+      XCTAssertEqual(output.intDict, ["en": 11, "es": 22])
+
+      // Episode
+      XCTAssertEqual(output.number, 42)
+      XCTAssertEqual(output.someOptional, "some optional")
+      XCTAssertEqual(output.someNil, nil)
+      XCTAssertEqual(output.date, Date(timeIntervalSince1970: 12345.0))
+      XCTAssertEqual(output.bool, true)
+      XCTAssertEqual(output.simpleStruct, SimpleStruct(foo: "foo!", bar: "bar?"))
+      XCTAssertEqual(output.simpleClass, SimpleClass(foo: "foo!", bar: "bar?"))
+      """)
+  }
+
+  static var allTests = [
+    ("testExample", testExample),
+  ]
+}
+
+struct SimpleStruct {
+  let foo: String = "foo!"
+  let bar: String? = "bar?"
+}
+
+struct SimpleClass {
+  let foo: String = "foo!"
+  let bar: String? = "bar?"
+}
+
+class Media {
+  let name: String = "Some media"
+  let array: [String] = ["foo", "bar"]
+  let stringDict: [String: String] = ["en": "Hi", "es": "Hola"]
+  let intDict: [String: Int] = ["en": 11, "es": 22]
+}
+
+class Episode: Media {
+  let number: Int = 42
+  let someOptional: String? = "some optional"
+  let someNil: String? = nil
+  let date: Date = Date(timeIntervalSince1970: 12345)
+  let bool: Bool = true
+  let simpleStruct = SimpleStruct()
+  let simpleClass = SimpleClass()
 }
